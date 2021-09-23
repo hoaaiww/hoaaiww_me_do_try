cb926818463475eb38cada693bf3ad2a461ffe1c
hoaaiww <75149340+hoaaiww@users.noreply.github.com>
2021-09-24 01:11:50 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,26 @@
+Config = {}
+
+Config.TextColor_Me         = { r = 255, g = 255, b = 255, a = 255 } -- Adjust the text color (3D and the Chat too)
+Config.TextColor_Do         = { r = 255, g = 255, b = 255, a = 255 }
+Config.TextColor_Try        = { r = 255, g = 255, b = 255, a = 255 }
+
+Config.Background           = true -- Enable/Disable the background of the 3D text?
+
+Config.BackgroundColor_Me   = { r = 63, g = 153, b = 247, a = 150 } -- Adjust the background color (3D and the Chat too)
+Config.BackgroundColor_Do   = { r = 202, g = 37, b = 78, a = 150 }
+Config.BackgroundColor_Try  = { r = 22, g = 173, b = 196, a = 150 }
+
+Config.DisplayOnChat        = true -- Display actions in chat?
+
+Config.EnablePlayerName     = true -- Enable player ingame name in the text?
+
+Config.Log                  = false -- Enable/Disable logging?
+Config.Logwebhook           = "Your Webhook Url" -- If logging enabled then replace 'Your Webhook' with your Discord Channel Webhook
+
+---------- Advanced options ----------
+
+Config.Duration             = 5 -- Text duration in seconds
+Config.Distance             = 20 -- Distance about who can see my action (in meter)
+Config.TryChance            = 50 -- Chance of the try action (in %)
+Config.DropShadow           = false -- Drop shadow (3D text)
+Config.Font                 = 4 -- Font type by number (3D text)
---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+game 'gta5'
+
+name 'ARP /ME,DO,TRY'
+description '/me, /do & /try commands'
+author 'hoaaiww'
+version 'v1.0'
+url 'https://discord.io/AntiQRolePlay'
+
+client_script {
+    'config.lua',
+    'client/client.lua'
+}
+
+server_script {
+    'config.lua',
+    '@mysql-async/lib/MySQL.lua',
+    'server/server.lua'
+}
