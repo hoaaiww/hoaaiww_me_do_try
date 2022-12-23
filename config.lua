97935afcdfce62450b6977a8829989d301a5aa05
hoaaiww <75149340+hoaaiww@users.noreply.github.com>
2022-12-23 21:55:35 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,26 +1,28 @@
-Config = {}
+Config = {} -- Do not touch this!
 
+
+Config.Background           = true -- Enable/Disable the background of the 3D text?
+Config.DisplayOnChat        = true -- Display actions in chat?
+
+Config.EnablePlayerName     = true -- Enable player ingame name in the text who executed the command?
+Config.UseESXRPName         = false -- Use ESX player names? If true you need to have running ESX (resource es_extended)!
+
+Config.Debug                = true -- print debugs in server console in case either error or information.
+
+Config.LogDiscord           = false -- Enable (true) / Disable (false) logging on Discord?
+Config.LogWebhook           = "Your Webhook Url" -- If "LogDiscord" is enabled then replace 'Your Webhook Url' with your Discord Channel Webhook.
+
+Config.checkForUpdates      = true -- Check for updates. /!\ Recommended turned on true
+Config.Version              = '2.0'
+
+---------- Advanced options ----------
 Config.TextColor_Me         = { r = 255, g = 255, b = 255, a = 255 } -- Adjust the text color (3D and the Chat too)
 Config.TextColor_Do         = { r = 255, g = 255, b = 255, a = 255 }
 Config.TextColor_Try        = { r = 255, g = 255, b = 255, a = 255 }
 
-Config.Background           = true -- Enable/Disable the background of the 3D text?
-
 Config.BackgroundColor_Me   = { r = 63, g = 153, b = 247, a = 150 } -- Adjust the background color (3D and the Chat too)
 Config.BackgroundColor_Do   = { r = 202, g = 37, b = 78, a = 150 }
 Config.BackgroundColor_Try  = { r = 22, g = 173, b = 196, a = 150 }
-
-Config.DisplayOnChat        = true -- Display actions in chat?
-
-Config.EnablePlayerName     = true -- Enable player ingame name in the text? If true you need to have ESX
-
-Config.Log                  = false -- Enable/Disable logging?
-Config.Logwebhook           = "Your Webhook Url" -- If logging enabled then replace 'Your Webhook' with your Discord Channel Webhook
-
-Config.checkForUpdates      = true -- Check for updates
-Config.Version              = '1.6'
-
----------- Advanced options ----------
 
 Config.Duration             = 5 -- Text duration (in seconds)
 Config.Distance             = 10 -- Who can see my action inside the distance (in meter)
